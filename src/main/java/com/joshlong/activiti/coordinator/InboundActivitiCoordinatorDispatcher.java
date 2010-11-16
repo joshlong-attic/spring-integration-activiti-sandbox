00182775be355e2596e440ea87f730a7d6927090
Josh Long <josh@joshlong.com>
2010-11-16 00:44:35 -0600

    more skeletal a structure is finally in place...

--- src/main/java/com/joshlong/activiti/coordinator/InboundActivitiCoordinatorDispatcher.java
+++ src/main/java/com/joshlong/activiti/coordinator/InboundActivitiCoordinatorDispatcher.java
@@ -1,10 +1,21 @@
+/*
+ * Copyright 2010 the original author or authors
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 package com.joshlong.activiti.coordinator;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
-import org.springframework.integration.MessagingException;
-import org.springframework.integration.core.MessageHandler;
 import org.springframework.integration.endpoint.AbstractEndpoint;
 import org.springframework.util.Assert;
 
@@ -14,8 +25,8 @@ import org.springframework.util.Assert;
  * Consumers will receive the work that matches both the specified <code>process_name</code> <em>and</em> <code>execution_id</code>.
  *
  * Thus, for the process called <code>customer-signup</code>, and the state
- * called <code>confirm-receipt</code>, an {@link com.joshlong.activiti.coordinator.ActivitiState}
- * annotation would specify something like:
+ * called <code>confirm-receipt</code>, an {@link com.joshlong.activiti.coordinator.annotations.ActivitiState}
+ * annotations would specify something like:
  *
  * <code>
  * &at;ActivitiState( processName = "customer-signup", state = "confirm-receipt")
@@ -63,14 +74,4 @@ public class InboundActivitiCoordinatorDispatcher  extends AbstractEndpoint {
 
 	}
 
-	/**
-	 * Simple dispatcher responsible for dispatching to
-	 * the appropriately registered {@link com.joshlong.activiti.coordinator.ActivitiHandler}
-	 * components.
-	 */
-	class ActivitiDispatcherMessageHandler implements MessageHandler {
-		public void handleMessage(Message<?> message) throws MessagingException {
-
-		}
-	}
 }
